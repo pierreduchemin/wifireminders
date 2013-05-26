747a8c3ece420db2ae00d3883af8f28c23b2cf9d
Alexander Inglessi <inglessi@gmail.com>
2013-05-26 14:20:42 +0400

    Removing unnecessary files

--- src/ru/glesik/wifireminders/RemindersListActivity.java
+++ src/ru/glesik/wifireminders/RemindersListActivity.java
@@ -1,16 +1,16 @@
 /*
  * This file is part of Wi-Fi Reminders.
- * 
+ *
  * Wi-Fi Reminders is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * Wi-Fi Reminders is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with Wi-Fi Reminders.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,10 +22,11 @@
  * starts AlarmService which checks for any of available networks match the reminders.
  * If yes, the notification is shown and active rules check is repeated. If there are no
  * active reminders, the AlarmManager is stopped.
- * 
+ *
  * TODO: Restart after reboot.
  * TODO: Add settings (sound, update frequency).
  * TODO: Pass active SSIDs with AlarmManager to minimize settings reads.
+ * TODO: Add current visible networks to the list when adding reminder.
  */
 
 package ru.glesik.wifireminders;
